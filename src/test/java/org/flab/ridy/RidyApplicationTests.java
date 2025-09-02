8350d878516e17316cd03cca14fba39068570cb3
jkjkh1318 <jkjkh1318@gmail.com>
2025-09-02 12:38:08 +0900

    first commit

---
+++ src/test/java/org/flab/ridy/RidyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.flab.ridy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
